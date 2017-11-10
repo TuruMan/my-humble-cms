8d62fe35921c09b40c009d5ccb63cb9f34b832d3
Zbynek <zbynek.jurica@gmail.com>
2017-11-10 20:45:27 +0100

    Commiting all files

---
+++ public/js/socket.js
@@ -0,0 +1,22 @@
+console.log('socket.jsiioooioi');
+const socket = io.connect(['http://192.168.100.244:3000', 'http://109.105.46.126:3000']);
+
+const button = document.getElementById('klik');
+const elementPocetKliknuti = document.getElementById('kliknuti');
+const elementPocetUzivatelu = document.getElementById('pocetUzivatelu');
+
+button.addEventListener('click', function() {
+  console.log('Click');
+  socket.emit('btn');
+});
+
+socket.on('btnServer', function(pocetKliknuti, socketClients){
+  console.log('Menim HTML');
+  elementPocetKliknuti.innerHTML = pocetKliknuti;
+  console.log(socketClients);
+});
+
+socket.on('updatePocetUzivatelu', function(pocetUzivatelu){
+  console.log('uzivatel pripojen ' + pocetUzivatelu);
+  elementPocetUzivatelu.innerHTML = pocetUzivatelu;
+});
